f9f04cd5d4f0ae226762df0312e4314ce210a836
Marc Kelsey <marc.kelsey@textnow.com>
2023-12-21 11:55:12 -0500

    Hello world

---
+++ 01-hello-world/hello.rs
@@ -0,0 +1,11 @@
+// This is a comment, and is ignored by the compiler.
+
+// This is the `main` function.
+fn main() {
+  // Statements here are executed when the compiled binary is called.
+
+  // Print some text to the console.
+  // `println` is a macro that prints text to the console.
+  println!("Hello World!");
+  println!("I'm a Rustacean");
+} 
